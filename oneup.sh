c735ca1f3504bfc6c3745d659278e66fc2e7fe5a
Lee <github@journalxtra.com>
2018-03-04 21:56:32 +0000

    Update to 1.0.1

--- oneup.sh
+++ oneup.sh
@@ -3,7 +3,7 @@ clear
 # set -x
 ###
 #
-#	OneUP! 1.0.0
+#	OneUP! 1.0.1
 #
 #	MAME and QMC2 installer & updater.
 #	Compiles both from source and installs the binaries and files to their expected locations.
@@ -14,7 +14,7 @@ clear
 #	Lead Author: Lee Hodson
 #	Donate: https://paypal.me/vr51
 #	Website: https://journalxtra.com/gaming/mame-qmc2-installer/
-#	This Release: 27th Feb. 2018
+#	This Release: 4th March. 2018
 #	First Written: 27th Feb. 2018
 #	First Release: 27th Feb. 2018
 #
@@ -27,14 +27,13 @@ clear
 #
 #	USE TO:
 #
-#	1) Install MAME from source
-#	2) Update MAME from source
-#	3) Install QMC2 from source
-#	4) Update QMC2 from source
-#	5) Update MAME default data files: artwork, bgfx, ctrlr, hash, keymaps, language, plugins, roms, samples.
-#	6) Create default output data directories: cfg nvram memcard inp sta snap diff comments
-#	7) Delete, or not, stale files before new files are downloaded. Option set affects only actions committed.
-#	8) Install packages required to successfully build MAME and QMC2. This option shows until used.
+#	1) Install/Update MAME from source (includes tools)
+#	2) Install/Update QMC2 from source (includes QMC2, QMC2 Arcade, QCHDMAN and the man help pages)
+#	3) Update MAME default data files: artwork, bgfx, ctrlr, hash, keymaps, language, plugins, roms and samples.
+#	4) Create default output data directories: cfg, nvram, memcard, inp, sta, snap, diff and comments
+#	5) Delete, or not, stale files before new files are downloaded. Option set affects only actions committed.
+#	6) Set the number of parallel jobs make should use during build process.
+#	7) Install packages required to successfully build MAME and QMC2. This option shows until used.
 #
 #	TO RUN:
 #
@@ -84,16 +83,18 @@ group=$(id -g -n $user) # Current user's primary group
 declare -a conf
 
 conf[0]=0 # Essentials # Install build essential software. 0 = Not done, 1 = Done
-conf[1]=0 # Clean Stale # Remove old build files before recompilation? 0/1. 0 = No, 1 = Yes.
-conf[2]=5 # CPU Cores # Number of CPU cores to employ to build binaries. Maximum value is total number of CPU cores + 1. More cores  = faster build time. If you have a quadcore, set to 4+1 i.e 5 cores.
-conf[3]=$(mame -? | grep 'MAME v') # Installed MAME Version
-conf[4]=$(grep 'INDEX - v' "/usr/local/share/qmc2/doc/html/us/index.html" | sed -E "s#</?font.{0,10}>##g" | tr -d '[:space:]') # Installed QMC2 Version
+conf[1]=2 # Clean Stale # Do no cleaning or run make clean or delete source files? 0/1/2. 0 = No, 1 = Soft, 2 = Hard.
+conf[2]=0 # Parallel jobs to run during build # Number of CPU cores + 1 is safe. Can be as high as 2*CPU cores. More jobs can shorten build time but not always and risks system stability. 0 = Auto.
+conf[3]=$(nproc) # Number of CPU cores the computer has.
+conf[4]=$(mame -? | grep 'MAME v') # Installed MAME Version
+conf[5]=$(grep 'INDEX - v' "/usr/local/share/qmc2/doc/html/us/index.html" | sed -E "s#[ ]{0,10}</?font.{0,10}>(INDEX - v)?##g") # Installed QMC2 Version
 
 
 ## END User Options
 
-# Set Menu Options
-declare -a menu=( 'Install MAME' 'Install QMC2' 'Update MAME' 'Update QMC2' 'Update MAME Data Files' 'Create MAME Data Directories' 'Remove Stale Source Files Before Builds' 'Install Essential Build Packages' )
+declare -a menu # Menu options are set within oneup_prompt()
+
+let safeproc=${conf[3]}+${conf[3]} # Safe number of parallel jobs, possibly.
 
 # Other settings
 
@@ -105,13 +106,13 @@ filepath="$( echo $PWD )"
 # A Little precaution
 cd "$filepath"
 
-
-# Make SRC directory
+# Make SRC directory if it does not already exist
 
 if test ! -d "$HOME/src"; then
 	mkdir "$HOME/src"
 fi
 
+# Functions
 
 function oneup_run() {
 	# Check for terminal then run else just run program
@@ -126,350 +127,394 @@ function oneup_run() {
 
 function oneup_prompt() {
 
-  while true; do
+	while true; do
 
-	if test ${conf[0]} == '1' ; then
-		unset menu[7] # Count starts at 0
-	fi
+		# Set Menu Options
 
-	printf $bold
-	printf "Menu\n\n"
-	printf $normal
+		case ${conf[1]} in
+		
+			0)
+				message[1]='No cleaning'
+				menu[1]='Update MAME'
+				menu[2]='Update QMC2'
+			;;
 
-	n=1
-	for i in "${menu[@]}"; do
-		printf "$n) $i\n"
-		let n=n+1
-	done
+			1)
+				message[1]='Clean Build Cache Only'
+				menu[1]='Update MAME'
+				menu[2]='Update QMC2'
+			;;
 
-	printf "\n0) Exit\n"
+			2)
+				message[1]='Delete Source Files'
+				menu[1]='Install MAME'
+				menu[2]='Install QMC2'
+			;;
 
-	# Notices
+		esac
 
-	printf $bold
-
-		if [ ${conf[0]} == 0 ]; then
-			printf "\nSuccessful build of MAME and QMC2 requires software packages installed by option 8. You should probably install these now. This message and Option 8 will not show once this is done.\n"
-		fi
-
-		printf "\nGeneral Settings\n"
-
-	printf $normal
-
-		if [ ${conf[1]} == 0 ]; then
-			printf "\nNo, do not delete old source files before action is committed."
-		else
-			printf "\nYes, delete old source files before action is committed."
-		fi
-
-		let cores=${conf[2]}-1
-		printf "\nUse $cores CPU cores to compile source.\n"
+		menu[3]=''
+		menu[4]='Update MAME Data Files'
+		menu[5]='Create MAME Data Directories'
+		menu[6]=''
+		
+		case "${conf[2]}" in
+		
+			0)
+				menu[7]="Number of parallel jobs the installer should run. ${conf[3]} is Safe. $safeproc Max: Auto"
+			;;
+			
+			*)
+				menu[7]="Number of parallel jobs the installer should run ( Auto, Safe(${conf[3]}) or Max($safeproc) ): ${conf[2]}"
+			;;
+			
+		esac
+		
+		menu[8]="Clean Level: ${message[1]}"
+		menu[9]=''
+		
+		case "${conf[0]}" in
+		
+			0)
+				menu[10]='Install Essential Build Packages'
+				message[1]='\nIf installation fails Install Essential Build Packages then try again.'
+			;;
+			
+		esac
+		
 
 		printf $bold
-			printf "\nGeneral Info\n"
+		printf "MENU\n\n"
 		printf $normal
 
-		printf "\nSystem MAME: ${conf[3]}"
-		printf "\nSystem QMC2: ${conf[4]}\n"
-
-	printf $bold
-		printf "\nChoose Wisely: "
-	printf $normal
-	read REPLY
-
-
-	case $REPLY in
-
-
-	1) # Install MAME
-
-		printf "\nInstalling MAME. This may take a few moments.\n"
-
-		cd "$HOME/src"
-
-		# Reset permissions & maybe start with a clean slate OR just download
-		if test -d "$HOME/src/mame" ; then
-
-			sudo chown -R $user:$group "$HOME/src/mame"
-
-			if [ $conf[1] == 1 ]; then
-
-				rm -r -f "$HOME/src/mame"
-				git clone "$mameloc"
-
+		n=1
+		for i in "${menu[@]}"; do
+			if [ "$i" == '' ]; then
+				printf "\n"
 			else
-
-				cd "$HOME/src/mame"
-				git pull -p
-
-			fi
-
-		else
-
-			git clone "$mameloc"
-
-		fi
-
-		cd "$HOME/src/mame"
-
-		make -j$cores TOOLS=1
-
-		if test -f "$HOME/src/mame/mame64"; then
-			sudo cp -f "$HOME/src/mame/mame64" "$mameinst/mame"
-		fi
-
-		if test -f "$HOME/src/mame/mame32"; then
-			sudo cp -f "$HOME/src/mame/mame32" "$mameinst/mame"
-		fi
-
-		if test -f "$HOME/src/mame/mame"; then
-			sudo cp -f "$HOME/src/mame/mame" "$mameinst/mame"
-		fi
-
-		sudo ldconfig
-		make clean
-
-		printf "\nMAME has been built from source and installed ready for use.\n"
-		printf "\nPress ANY key\n"
-		read something
-		clear
-
-	;;
-
-	2) # Install QMC2
-
-		printf "\nInstalling QMC2. This may take a few moments.\n"
-
-		cd "$HOME/src"
-
-		# Reset permissions & maybe start with a clean slate
-		if test -d "$HOME/src/qmc2" ; then
-			sudo chown -R $user:$group "$HOME/src/qmc2"
-			if [ $conf[1] == 1 ]; then
-				rm -r -f "$HOME/src/qmc2"
-			fi
-		fi
-
-		# Download, build and install
-		svn co "$qmc2loc" qmc2
-		cd "$HOME/src/qmc2"
-
-		make -j${conf[2]} DISTCFG=1
-		sudo make install DISTCFG=1
-		make -j${conf[2]} arcade DISTCFG=1
-		sudo make arcade-install DISTCFG=1
-		make -j${conf[2]} qchdman DISTCFG=1
-		sudo make qchdman-install DISTCFG=1
-		make -j${conf[2]} man
-		sudo make man-install
-		make distclean DISTCFG=1
-		sudo ldconfig
-		make clean
-
-		printf "\nQMC2 has been built from source and installed ready for use.\n"
-		read something
-		clear
-
-	;;
-
-	3) # Update MAME -- Currently the same as initial install process
-
-		printf "\nUpdating MAME. This may take a few moments.\n"
-
-		cd "$HOME/src"
-
-		# Reset permissions & maybe start with a clean slate OR just download them
-		if test -d "$HOME/src/mame" ; then
-
-			sudo chown -R $user:$group "$HOME/src/mame"
-
-			if [ $conf[1] == 1 ]; then
-
-				rm -r -f "$HOME/src/mame"
-				git clone "$mameloc"
-
-			else
-
-				cd "$HOME/src/mame"
-				git pull -p
-
-			fi
-
-		else
-
-			git clone "$mameloc"
-
-		fi
-
-		cd "$HOME/src/mame"
-
-		make -j${conf[2]} TOOLS=1
-
-		if test -f "$HOME/src/mame/mame64"; then
-			sudo cp -f "$HOME/src/mame/mame64" "$mameinst/mame"
-		fi
-
-		if test -f "$HOME/src/mame/mame32"; then
-			sudo cp -f "$HOME/src/mame/mame32" "$mameinst/mame"
-		fi
-
-		if test -f "$HOME/src/mame/mame"; then
-			sudo cp -f "$HOME/src/mame/mame" "$mameinst/mame"
-		fi
-
-		sudo ldconfig
-		make clean
-
-		printf "\nMAME has been rebuilt from source and installed ready for use.\n"
-		printf "\nPress ANY key\n"
-		read something
-		clear
-
-	;;
-
-	4) # Update QMC2
-
-		printf "\nUpdating QMC2. This may take a few moments.\n"
-
-		# Update package files, build and install
-		cd "$HOME/src/qmc2"
-		svn update
-
-		make -j${conf[2]} DISTCFG=1
-		sudo make install DISTCFG=1
-		make -j${conf[2]} arcade DISTCFG=1
-		sudo make arcade-install DISTCFG=1
-		make -j${conf[2]} qchdman DISTCFG=1
-		sudo make qchdman-install DISTCFG=1
-		make -j${conf[2]} man
-		sudo make man-install
-		make distclean DISTCFG=1
-		sudo ldconfig
-		make clean
-
-		printf "\nQMC2 has been updated ready for use."
-		printf "\nRemember to clear QMC2 software caches from QMC2 > Tools > Clean Up > Clear All Emulator Caches."
-		printf "\nAlternatively, launch QMC2 from the command line with$bold qmc2 -cc$normal\n"
-		printf "\nPress ANY key\n"
-		read something
-		clear
-
-	;;
-
-	5) # Update MAME data files
-
-		printf "\nUpdating MAME data files. This shouldn't take long.\n"
-
-		cd "$HOME/src"
-
-		# Reset permissions & maybe start with a clean slate OR just download them
-		if test -d "$HOME/src/mame" ; then
-
-			sudo chown -R $user:$group "$HOME/src/mame"
-
-			if [ $conf[1] == 1 ]; then
-
-				rm -r -f "$HOME/src/mame"
-				git clone "$mameloc"
-
-			else
-
-				cd "$HOME/src/mame"
-				git pull -p
-
-			fi
-
-		else
-
-			git clone "$mameloc"
-
-		fi
-
-		cd "$HOME/src/mame"
-
-		# Copy data files to where needed
-		unset files
-		files=( artwork bgfx ctrlr hash keymaps language plugins roms samples )
-		for i in "${files[@]}"; do
-			cp -f -R "$HOME/src/mame/$i" "$HOME/.mame/"
-			sudo cp -f -R "$HOME/src/mame/$i" "$mamesearchpath/"
-			sudo cp -f -R "$HOME/src/mame/$i" "$mamesearchpath2/"
-		done
-
-		sudo cp -f -R "$HOME/src/mame/ini/presets" "/etc/mame/"
-
-		printf "\nMAME basic data files have been copied to $HOME/.mame\n"
-		printf "\nIf you use QMC2 you will need to clean its software caches to reinitialise the lists of usable software and ROMs. Newly available software will not display in the QMC2 software lists until the caches are cleaned.\n"
-		printf "\nClear QMC2 software caches from QMC2 > Tools > Clean Up > Clear All Emulator Caches."
-		printf "\nAlternatively, launch QMC2 from the command line with$bold qmc2 -cc$normal\n"
-		printf "\nPress ANY key\n"
-		read something
-		clear
-
-	;;
-
-
-	6) # Create MAME data output directories
-
-		printf "\nCreating MAME output data directories.\n"
-
-		# Create default data output directories unless they already exist
-		unset files
-		files=( cfg nvram memcard inp sta snap diff comments )
-		for i in "${files[@]}"; do
-			if test ! -d "$HOME/.mame/$i"; then
-				mkdir "$HOME/.mame/$i"
+				printf "$n) $i\n"
+				let n=n+1
 			fi
 		done
 
-		printf "\nMAME data output directories have been added to $HOME/.mame\n"
-		printf "\nPress ANY key\n"
-		read something
-		clear
+		printf "\n0) Exit\n"
 
-	;;
+		# Notices
 
-	7) # Download Fresh Source Files
+			printf $bold
 
-		if test ${conf[1]} == 0; then
-			sed -i -E "0,/conf\[1\]=0/s/conf\[1\]=0/conf\[1\]=1/" "$0"
-			conf[1]=1
-		else
-			sed -i -E "0,/conf\[1\]=1/s/conf\[1\]=1/conf\[1\]=0/" "$0"
-			conf[1]=0
-		fi
+			printf "${message[1]}"
+			printf "\nIf the computer crashes during installation lower the number of parallel jobs used by the installer then try again.\n"
 
-		clear
+			printf "\nGENERAL INFO\n"
+				
+			printf $normal
 
-	;;
+			printf "\nRestart OneUp! if this looks wrong.\n"
+			printf "\nSystem MAME: ${conf[4]}"
+			printf "\nSystem QMC2: ${conf[5]}\n"
+			
+			printf "\nIf you use QMC2 you will need to clean its software caches after any updates have been committed otherwise the QMC2 playable software lists and playable ROM lists will be out-of-date.\n"
+			printf "\nClear QMC2 software caches from QMC2 > Tools > Clean Up > Clear All Emulator Caches or launch QMC2 from the command line with$bold qmc2 -cc$normal\n"
 
-	8) # Install Build Essential Packages
+		printf $bold
+			printf "\nChoose Wisely: "
+		printf $normal
+		read REPLY
 
-		sudo apt-get update
-		packages=( build-essential subversion g++ libqtwebkit-dev libphonon-dev libxmu-dev rsync libfontconfig-dev libsdl2* libqt5* qt5* )
-		for i in "${packages[@]}"; do
-			sudo apt-get build-dep -y -q $i
-			sudo apt-get install -y -q --install-suggests $i
-		done
+		case $REPLY in
 
-		sed -i -E "0,/conf\[0\]=0/s/conf\[0\]=0/conf\[0\]=1/" "$0"
-		conf[0]=1
+		1) # Install / Update MAME
 
-		printf "\nPress any key to continue\n"
-		read something
-		clear
+			printf "\nInstalling MAME. This may take a few moments to a long time. Go get a coffee.\n"
 
-	;;
+			cd "$HOME/src"
 
-	0) # Exit
+			# Test the mame source files exist. Download them if not.
+			if test -d "$HOME/src/mame" ; then
+				# Make sure we own the source files
+				sudo chown -R $user:$group "$HOME/src/mame"
+				
+				# Decide whether to update or install
+				case $conf[1] in
+				
+					0) # Update - No spring clean. Update source files
+						cd "$HOME/src/mame"
+						git pull -p
+					;;
+				
+					1) # Update - spring clean first. Update source files
+						cd "$HOME/src/mame"
+						make clean
+						make distclean
+						git pull -p
+					;;
+					
+					2) # Clean install. Delete source files. Download fresh source files.
+						rm -r -f "$HOME/src/mame"
+						git clone "$mameloc"
+						cd "$HOME/src/mame"
+					;;
+					
+				esac
 
-		exit 0
+			else
+				# Clean install necessary - Source files not present yet
+				git clone "$mameloc"
+				cd "$HOME/src/mame"
 
-	;;
+			fi
 
-	*)
+			case "${conf[2]}" in
+				0)
+					jobs=''
+				;;
+				
+				*)
+					jobs="-j${conf[2]}"
+				;;
+			esac
+			
+			# Build MAME
+			make $jobs TOOLS=1
 
-	esac
+			# Install MAME
+			if test -f "$HOME/src/mame/mame64"; then
+				sudo cp -f "$HOME/src/mame/mame64" "$mameinst/mame"
+			fi
+
+			if test -f "$HOME/src/mame/mame32"; then
+				sudo cp -f "$HOME/src/mame/mame32" "$mameinst/mame"
+			fi
+
+			if test -f "$HOME/src/mame/mame"; then
+				sudo cp -f "$HOME/src/mame/mame" "$mameinst/mame"
+			fi
+
+			sudo ldconfig
+
+			printf "\nMAME is ready to use.\n"
+			printf "\nPress ANY key"
+			read something
+			clear
+
+		;;
+
+		2) # Install / Update QMC2
+
+			printf "\nInstalling QMC2. This may take a few moments.\n"
+
+			cd "$HOME/src"
+
+			# Check whether QMC2 source files already exist. Download them if not.
+			if test -d "$HOME/src/qmc2" ; then
+				# Make sure we own the source files
+				sudo chown -R $user:$group "$HOME/src/qmc2"
+				
+				# Decide whether to update or install
+				case $conf[1] in
+				
+					0) # Update. No spring clean. Update source files
+						cd "$HOME/src/qmc2"
+						svn update
+					;;
+
+					1) # Update. Do spring clean. Update source files
+						cd "$HOME/src/qmc2"
+						make clean
+						make distclean DISTCFG=1
+						svn update
+					;;
+					
+					2) # Install. Delete existing source files. Download fresh source files.
+						rm -r -f "$HOME/src/qmc2"
+						svn co "$qmc2loc" qmc2
+					;;
+					
+				esac
+				
+			else
+				# Download fresh source files. Files do not not present yet.
+				svn co "$qmc2loc" qmc2
+
+			fi
+
+			case "${conf[2]}" in
+				0)
+					jobs=''
+				;;
+				
+				*)
+					jobs="-j${conf[2]}"
+				;;
+			esac
+			
+			# Build & install QMC2, QMC2 Arcade and QCHDMAN and man pages.
+			cd "$HOME/src/qmc2"
+
+			make -j${conf[2]} DISTCFG=1
+			sudo make $jobs install DISTCFG=1
+			make -j${conf[2]} arcade DISTCFG=1
+			sudo make $jobs arcade-install DISTCFG=1
+			make -j${conf[2]} qchdman DISTCFG=1
+			sudo make $jobs qchdman-install DISTCFG=1
+			make -j${conf[2]} man
+			sudo make $jobs man-install
+			sudo ldconfig
+
+			printf "\nQMC2 is ready to use.\n"
+			printf "\nPress ANY key"
+			read something
+			clear
+
+		;;
+
+		3) # Update MAME data files
+
+			printf "\nUpdating MAME data files. This shouldn't take long.\n"
+
+			cd "$HOME/src"
+
+			# Reset permissions & maybe start with a clean slate OR just download them
+			if test -d "$HOME/src/mame" ; then
+
+				sudo chown -R $user:$group "$HOME/src/mame"
+
+				if [ $conf[1] == 1 ]; then
+
+					rm -r -f "$HOME/src/mame"
+					git clone "$mameloc"
+
+				else
+
+					cd "$HOME/src/mame"
+					git pull -p
+
+				fi
+
+			else
+
+				git clone "$mameloc"
+
+			fi
+
+			cd "$HOME/src/mame"
+
+			# Copy data files to where needed
+			unset files
+			files=( artwork bgfx ctrlr hash keymaps language plugins roms samples )
+			for i in "${files[@]}"; do
+				cp -f -R "$HOME/src/mame/$i" "$HOME/.mame/"
+				sudo cp -f -R "$HOME/src/mame/$i" "$mamesearchpath/"
+				sudo cp -f -R "$HOME/src/mame/$i" "$mamesearchpath2/"
+			done
+
+			sudo cp -f -R "$HOME/src/mame/ini/presets" "/etc/mame/"
+
+			printf "\nMAME basic data files have been copied to $HOME/.mame\n"
+			printf "\nPress ANY key\n"
+			read something
+			clear
+
+		;;
+
+
+		4) # Create MAME data output directories
+
+			printf "\nCreating MAME output data directories.\n"
+
+			# Create default data output directories unless they already exist
+			unset files
+			files=( cfg nvram memcard inp sta snap diff comments )
+			for i in "${files[@]}"; do
+				if test ! -d "$HOME/.mame/$i"; then
+					mkdir "$HOME/.mame/$i"
+				fi
+			done
+
+			printf "\nMAME data output directories have been added to $HOME/.mame\n"
+			printf "\nPress ANY key\n"
+			read something
+			clear
+
+		;;
+		
+		5) # CPU Cores to Use
+		
+			case "${conf[2]}" in
+			
+				$safeproc)
+
+					let conf[2]=0
+					sed -i -E "0,/conf\[2\]=[0-9]{1,2}/s/conf\[2\]=[0-9]{1,2}/conf\[2\]=${conf[2]}/" "$0"
+
+				;;
+
+				*)
+
+					let conf[2]=${conf[2]}+1
+					sed -i -E "0,/conf\[2\]=[0-9]{1,2}/s/conf\[2\]=[0-9]{1,2}/conf\[2\]=${conf[2]}/" "$0"
+					
+				;;
+
+			esac
+
+			clear
+
+		;;
+
+		6) # Set update, install, clean flag
+		
+			case ${conf[1]} in
+			
+				0)
+					sed -i -E "0,/conf\[1\]=0/s/conf\[1\]=0/conf\[1\]=1/" "$0"
+					conf[1]=1
+				;;
+
+				1)
+					sed -i -E "0,/conf\[1\]=1/s/conf\[1\]=1/conf\[1\]=2/" "$0"
+					conf[1]=2
+				;;
+
+				2)
+					sed -i -E "0,/conf\[1\]=2/s/conf\[1\]=2/conf\[1\]=0/" "$0"
+					conf[1]=0
+				;;
+
+			esac
+
+			clear
+			
+		;;
+
+		7) # Install software packages necessary to build MAME and QMC2
+
+			sudo apt-get update
+			packages=( build-essential subversion g++ libqtwebkit-dev libphonon-dev libxmu-dev rsync libfontconfig-dev libsdl2* libqt5* qt5* )
+			for i in "${packages[@]}"; do
+				sudo apt-get build-dep -y -q $i
+				sudo apt-get install -y -q --install-suggests $i
+			done
+
+			sed -i -E "0,/conf\[0\]=0/s/conf\[0\]=0/conf\[0\]=1/" "$0"
+			conf[0]=1
+
+			printf "\nPress any key to continue\n"
+			read something
+			clear
+
+		;;
+
+		0) # Exit
+
+			exit 0
+
+		;;
+
+		*)
+
+		esac
 
   done
   
